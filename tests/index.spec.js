b6d303b001ced8df99f7cf75c8ecae22dff1abed
Guilherme Somenzi <somenziguilherme@gmail.com>
2026-05-29 13:38:31 -0300

    fix: remoção de teste desnecessário

--- tests/index.spec.js
+++
@@ -1,8 +0,0 @@
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const src1 = require("../src");
-describe('sum', () => {
-    it('should sum two numbers', () => {
-        expect(src1.sum(1, 2)).toBe(3);
-    });
-});
