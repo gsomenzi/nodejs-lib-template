274d9d61ca8ca6585b235e89e970780fe08baa9f
Guilherme Somenzi <somenziguilherme@gmail.com>
2026-05-29 13:36:07 -0300

    fix: correção de teste padrão

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,8 @@
-"use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
+const src1 = require("../src");
 describe('sum', () => {
     it('should sum two numbers', () => {
-        expect((0, src_1.sum)(1, 2)).toBe(3);
+        expect(src1.sum(1, 2)).toBe(3);
     });
 });
